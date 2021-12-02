25f7854b22f4ef6bfdb0bf11ec2fd043f9bf2eac
Decratos <57159768+Decratos@users.noreply.github.com>
2021-12-02 20:14:23 +0100

    Modification du troisième étage du centre et mise en place de la base du système d'armement

--- Assets/Scripts/WeaponsClass.cs
+++ Assets/Scripts/WeaponsClass.cs
@@ -5,13 +5,43 @@ using UnityEngine;
 public class WeaponsClass : MonoBehaviour
 {
 
+    [Header("Gathering Attributes")]
     public bool canCutWood;
     public bool canCutStone;
+
+    [Header("General Attributes")]
     public int damage;
     public float attackSpeed;
+    public bool rangedWeapon;
+    public float attackDistance;
+    public float attackRate;
+
+    [Header("Ranged Weapon Attributes"), Tooltip("N'a d'impact que si l'arme est à distance")]
+    public int ammo;
+    public int maxAmmo;
+    public int reloadSpeed;
     public virtual void Shoot()
     {
-        
+        if(rangedWeapon)
+        {
+            if(Physics.Raycast(transform.position, transform.forward, out RaycastHit hit, Mathf.Infinity))
+            {
+                if(hit.transform.CompareTag("NPC"))
+                {
+
+                }
+            }
+        }
+        else
+        {
+            if(Physics.Raycast(transform.position, transform.forward, out RaycastHit hit, attackDistance))
+            {
+                if(hit.transform.CompareTag("NPC"))
+                {
+                    
+                }
+            }
+        }
     }
 
     public virtual void Reload()
