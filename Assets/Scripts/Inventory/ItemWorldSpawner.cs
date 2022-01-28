e595e8d237e9f58ae1a7c9ab4e7e64cdfcf18b10
Decratos <57159768+Decratos@users.noreply.github.com>
2022-01-28 21:11:08 +0100

    Mise à jour du spawner d'item
    Ajout de sélection du type d'objet souhaité, ajout d'une chance à setup afin que l'objet spawn ou non

--- Assets/Scripts/Inventory/ItemWorldSpawner.cs
+++ Assets/Scripts/Inventory/ItemWorldSpawner.cs
@@ -6,16 +6,69 @@ public class ItemWorldSpawner : MonoBehaviour
 {
 
     public ItemClass item;
+    [SerializeField] private ResumeExelForObject.Type type;
+    [SerializeField] private float chanceForNoSpawn;
     
     // Start is called before the first frame update
     void Start()
     {
+        if (CheckNoSpawnParameter())
+        {
+            InitSpawnMecanism();
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void InitSpawnMecanism()
+    {
         liseurExel excelLiseur;
         MesFonctions.FindDataExelForObject(out excelLiseur);
+        item = ChooseAnItem();
         excelLiseur.findObjectIDByName(item.itemName, out item.spriteId);
         var myItem = ItemWorld.SpawnItemWorld(item, transform.position);
         myItem.transform.parent = transform.parent;
-        Destroy(this);
+        Destroy(this.gameObject);
     }
     
+
+    private ItemClass ChooseAnItem()
+    {
+        bool hasFindItem = false;
+        int ID = 0;
+        string _name = "";
+        InfoGlobalExel _info = new InfoGlobalExel();
+        while (!hasFindItem)
+        {
+            ID = Random.Range(0, 79);
+            liseurExel.LesDatas.findObjectNameByID(ID, out _name);
+            liseurExel.LesDatas.FindObjectInfo(_name, out _info);
+            print(_info.TypeGeneral.ToString() + " " + _info.Name);
+
+            if (_info.TypeGeneral.ToString() == type.ToString())
+            {
+                item = new ItemClass { itemName = _info.Name, globalInfo = _info, itemType = type, spriteId = _info.ID };
+                hasFindItem = true;
+            }
+            
+        }
+        return item;
+    }
+    private bool CheckNoSpawnParameter()
+    {
+        float A = Random.Range(0f, 1f);
+        if (A <= chanceForNoSpawn)
+        {
+            return false;
+            print("Pas de spawn");
+        }
+        else
+        {
+            print("Spawn possible");
+            return true;
+        }
+            
+    }
 }
